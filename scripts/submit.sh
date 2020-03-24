c35a5cd25c249e24bee142cd67f2ff906a0ef280
Marek Cermak <macermak@redhat.com>
2020-03-24 19:58:51 +0100

    Added a script to submit the Workflow
    Signed-off-by: Marek Cermak <macermak@redhat.com>
    
    new file:   scripts/submit.sh

---
+++ scripts/submit.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -x
+
+set -eo pipefail
+
+STANDARD_IMAGES=$(cat "config/standard-images.json")
+
+
+main() {
+	: "${1?Positional argument namespace was not provided}"
+	: "${2?Positional argument apiserver was not provided}"
+	: "${TOKEN?Muset set TOKEN env var}"
+
+	local namespace="$1"
+	local apiserver="$2"
+
+	docker run \
+		-v "$(pwd)/openshift":/tmp:ro  \
+		argoproj/argocli:v2.4.3 submit \
+		--token "$TOKEN"                               \
+		--server    "$apiserver"                       \
+		--namespace "$namespace"                       \
+		--parameter standard-images="$STANDARD_IMAGES" \
+		--insecure-skip-tls-verify					   \
+		--wait /tmp/notebook-builder-workflow.yaml
+}
+
+main $@
