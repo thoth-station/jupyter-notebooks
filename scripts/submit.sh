0d0ad3e022c7daeaf8d62a8ad6d05b364bc8e45e
Marek Cermak <macermak@redhat.com>
2020-03-26 17:48:49 +0100

    Argo submission script no longer requires token
    The script will be run in an authenticated environment
    
    Signed-off-by: Marek Cermak <macermak@redhat.com>
    
    new file:   actions-runner/Dockerfile
    new file:   actions-runner/openshift/deployment-template.yaml
    modified:   scripts/submit.sh

--- scripts/submit.sh
+++ scripts/submit.sh
@@ -4,26 +4,24 @@ set -x
 
 set -eo pipefail
 
+
 STANDARD_IMAGES=$(cat "config/standard-images.json")
 
-
 main() {
-	: "${1?Positional argument namespace was not provided}"
-	: "${2?Positional argument apiserver was not provided}"
-	: "${TOKEN?Muset set TOKEN env var}"
-
-	local namespace="$1"
-	local apiserver="$2"
-
-	docker run \
-		-v "$(pwd)/workflows":/tmp:ro  \
-		argoproj/argocli:v2.4.3 submit \
-		--token "$TOKEN"                               \
-		--server    "$apiserver"                       \
-		--namespace "$namespace"                       \
-		--parameter standard-images="$STANDARD_IMAGES" \
-		--insecure-skip-tls-verify					   \
-		--wait /tmp/notebook-builder-workflow.yaml
+	USE_DOCKER=${USE_DOCKER:-0}
+	if [ $USE_DOCKER != 0]; then
+		docker run \
+			-v "$(pwd)/workflows":/tmp:ro  \
+			argoproj/argocli:v2.4.3 submit \
+			--parameter standard-images="$STANDARD_IMAGES" \
+			--insecure-skip-tls-verify					   \
+			--wait $@ https://raw.githubusercontent.com/argoproj/argo/master/examples/hello-world.yaml
+	else
+			argo submit \
+			--parameter standard-images="$STANDARD_IMAGES" \
+			--insecure-skip-tls-verify					   \
+			--wait $@ https://raw.githubusercontent.com/argoproj/argo/master/examples/hello-world.yaml
+	fi
 }
 
 main $@
